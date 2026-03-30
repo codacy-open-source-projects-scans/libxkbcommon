130c0264a1a6d71fdd017723245195d8535e8852
Codacy Archive <codacy-archive@local>
2026-03-30 00:15:31 +0000

    snapshot 2026-03-30T00:15:31.159957+00:00

--- test/features.c
+++ test/features.c
@@ -8,6 +8,7 @@
 #include "test-config.h"
 
 #include <limits.h>
+#include <stdint.h>
 
 #include "xkbcommon/xkbcommon-features.h"
 #include "test.h"
@@ -31,33 +32,33 @@ test_libxkbcommon_enums(void)
         enum xkb_feature feature;
         enum enum_property properties;
         const char * name;
-        const int * values;
+        const uint32_t * values;
         size_t count;
     } tests[] = {
 #define ENUM(feature, values, flag) { (feature), (flag), STRINGIFY(feature), (values), ARRAY_SIZE(values) }
         ENUM(XKB_FEATURE_ENUM_FEATURE, xkb_feature_values, ENUM_NONE),
+        ENUM(XKB_FEATURE_ENUM_ERROR_CODE, xkb_error_code_values, ENUM_NONE),
         ENUM(XKB_FEATURE_ENUM_CONTEXT_FLAGS, xkb_context_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_LOG_LEVEL, xkb_log_level_values, ENUM_NONE),
-        ENUM(XKB_FEATURE_ENUM_ERROR_CODE, xkb_error_code_values, ENUM_NONE),
+        ENUM(XKB_FEATURE_ENUM_KEYSYM_FLAGS, xkb_keysym_flags_values, ENUM_FLAG),
+        ENUM(XKB_FEATURE_ENUM_RMLVO_BUILDER_FLAGS, xkb_rmlvo_builder_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_KEYMAP_FORMAT, xkb_keymap_format_values, ENUM_NONE),
         ENUM(XKB_FEATURE_ENUM_KEYMAP_COMPILE_FLAGS, xkb_keymap_compile_flags_values, ENUM_FLAG),
-        ENUM(XKB_FEATURE_ENUM_RMLVO_BUILDER_FLAGS, xkb_rmlvo_builder_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_KEYMAP_SERIALIZE_FLAGS, xkb_keymap_serialize_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_KEYMAP_KEY_ITERATOR_FLAGS, xkb_keymap_key_iterator_flags_values, ENUM_FLAG),
-        ENUM(XKB_FEATURE_ENUM_KEYSYM_FLAGS, xkb_keysym_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_STATE_COMPONENT, xkb_state_component_values, ENUM_FLAG),
-        ENUM(XKB_FEATURE_ENUM_KEYBOARD_CONTROL_FLAGS, xkb_keyboard_control_flags_values, ENUM_FLAG),
-        ENUM(XKB_FEATURE_ENUM_A11Y_FLAGS, xkb_a11y_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_LAYOUT_OUT_OF_RANGE_POLICY, xkb_layout_out_of_range_policy_values, ENUM_NONE),
-        ENUM(XKB_FEATURE_ENUM_EVENT_TYPE, xkb_event_type_values, ENUM_NONE),
-        ENUM(XKB_FEATURE_ENUM_EVENTS_FLAGS, xkb_events_flags_values, ENUM_FLAG),
-        ENUM(XKB_FEATURE_ENUM_KEY_DIRECTION, xkb_key_direction_values, ENUM_NONE),
+        ENUM(XKB_FEATURE_ENUM_A11Y_FLAGS, xkb_a11y_flags_values, ENUM_FLAG),
+        ENUM(XKB_FEATURE_ENUM_KEYBOARD_CONTROL_FLAGS, xkb_keyboard_control_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_STATE_MATCH, xkb_state_match_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_CONSUMED_MODE, xkb_consumed_mode_values, ENUM_NONE),
+        ENUM(XKB_FEATURE_ENUM_EVENT_TYPE, xkb_event_type_values, ENUM_NONE),
+        ENUM(XKB_FEATURE_ENUM_KEY_DIRECTION, xkb_key_direction_values, ENUM_NONE),
+        ENUM(XKB_FEATURE_ENUM_EVENTS_FLAGS, xkb_events_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_COMPOSE_FORMAT, xkb_compose_format_values, ENUM_NONE),
         ENUM(XKB_FEATURE_ENUM_COMPOSE_COMPILE_FLAGS, xkb_compose_compile_flags_values, ENUM_FLAG),
-        ENUM(XKB_FEATURE_ENUM_COMPOSE_STATE_FLAGS, xkb_compose_state_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_COMPOSE_STATUS, xkb_compose_status_values, ENUM_NONE),
+        ENUM(XKB_FEATURE_ENUM_COMPOSE_STATE_FLAGS, xkb_compose_state_flags_values, ENUM_FLAG),
         ENUM(XKB_FEATURE_ENUM_COMPOSE_FEED_RESULT, xkb_compose_feed_result_values, ENUM_NONE),
 #undef  ENUM
     };
@@ -73,15 +74,15 @@ test_libxkbcommon_enums(void)
 
         /* Explicit values */
         bool has_zero = false;
-        int min = INT_MAX;
-        int max = INT_MIN;
+        int32_t min = INT32_MAX;
+        int32_t max = INT32_MIN;
         assert(tests[t].count > 0);
         for (size_t v = 0; v < tests[t].count; v++) {
-            assert(xkb_has_feature(tests[t].feature, tests[t].values[v]));
+            assert(xkb_feature_supported(tests[t].feature, tests[t].values[v]));
             if (tests[t].values[v] == 0)
                 has_zero = true;
-            min = MIN(min, tests[t].values[v]);
-            max = MAX(max, tests[t].values[v]);
+            min = MIN(min, (int32_t)tests[t].values[v]);
+            max = MAX(max, (int32_t)tests[t].values[v]);
         }
 
         const enum xkb_feature feature = tests[t].feature;
@@ -89,34 +90,35 @@ test_libxkbcommon_enums(void)
             /* Flag enum */
 
             /* Explicit zero */
-            assert(has_zero ^ !xkb_has_feature(feature, 0));
+            assert(has_zero ^ !xkb_feature_supported(feature, 0));
             /* No negative values */
             assert(min >= 0);
-            assert(!xkb_has_feature(feature, -1));
-            assert(!xkb_has_feature(feature, INT_MIN));
+            assert(!xkb_feature_supported(feature, -1));
+            assert(!xkb_feature_supported(feature, INT_MIN));
+            assert(!xkb_feature_supported(feature, INT32_MIN));
             /* No high positive values */
             #define ENUM_HIGHEST_VALUE XKB_STATE_MATCH_NON_EXCLUSIVE
             assert(max <= ENUM_HIGHEST_VALUE);
             /* Invalid mask */
-            static_assert(ENUM_HIGHEST_VALUE < (INT_MAX >> 1), "");
-            assert(!xkb_has_feature(feature, (ENUM_HIGHEST_VALUE << 1)));
+            static_assert(ENUM_HIGHEST_VALUE < (INT32_MAX >> 1), "");
+            assert(!xkb_feature_supported(feature, (ENUM_HIGHEST_VALUE << 1)));
             #undef ENUM_HIGHEST_VALUE
             if (max > 0) {
-                assert(!xkb_has_feature(feature, (max << 1)));
-                assert(!xkb_has_feature(feature, max | (max << 1)));
+                assert(!xkb_feature_supported(feature, (max << 1)));
+                assert(!xkb_feature_supported(feature, max | (max << 1)));
             }
         } else {
             /* Non-flag enum */
 
             /* Explicit zero */
-            assert(has_zero ^ !xkb_has_feature(feature, 0));
+            assert(has_zero ^ !xkb_feature_supported(feature, 0));
             /* No high values */
             assert(min > -2);
-            assert(!xkb_has_feature(feature, -2));
-            assert(!xkb_has_feature(feature, min - 1));
+            assert(!xkb_feature_supported(feature, -2));
+            assert(!xkb_feature_supported(feature, min - 1));
             assert(max < 0xf000);
-            assert(!xkb_has_feature(feature, 0xf000));
-            assert(!xkb_has_feature(feature, max + 1));
+            assert(!xkb_feature_supported(feature, 0xf000));
+            assert(!xkb_feature_supported(feature, max + 1));
         }
     }
 
@@ -125,20 +127,20 @@ test_libxkbcommon_enums(void)
      */
 
     /* Invalid feature */
-    assert(!xkb_has_feature(-1, 0));
-    assert(!xkb_has_feature(0xffff, 0));
+    assert(!xkb_feature_supported(-1, 0));
+    assert(!xkb_feature_supported(0xffff, 0));
     /* Invalid enum value */
-    assert(!xkb_has_feature(XKB_FEATURE_ENUM_FEATURE, -1));
-    assert(!xkb_has_feature(XKB_FEATURE_ENUM_FEATURE, 0xffff));
+    assert(!xkb_feature_supported(XKB_FEATURE_ENUM_FEATURE, -1));
+    assert(!xkb_feature_supported(XKB_FEATURE_ENUM_FEATURE, 0xffff));
 
     /*
      * Specific values
      */
 
-    assert(!xkb_has_feature(XKB_FEATURE_ENUM_KEYMAP_FORMAT,
+    assert(!xkb_feature_supported(XKB_FEATURE_ENUM_KEYMAP_FORMAT,
                             XKB_KEYMAP_USE_ORIGINAL_FORMAT));
-    assert(!xkb_has_feature(XKB_FEATURE_ENUM_KEYMAP_FORMAT, 0));
-    assert(!xkb_has_feature(XKB_FEATURE_ENUM_KEYMAP_FORMAT, 3));
+    assert(!xkb_feature_supported(XKB_FEATURE_ENUM_KEYMAP_FORMAT, 0));
+    assert(!xkb_feature_supported(XKB_FEATURE_ENUM_KEYMAP_FORMAT, 3));
 }
 
 int
